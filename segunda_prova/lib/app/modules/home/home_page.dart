a144f95b7675a9c0f0ad78a2fd3cf7e2cc342aa7
caio-cruz-garcia <61151058+caio-cruz-garcia@users.noreply.github.com>
2020-11-24 21:14:18 -0300

    Api.dart
    Criação de models e api

--- segunda_prova/lib/app/modules/home/home_page.dart
+++ segunda_prova/lib/app/modules/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,12 +17,16 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      color: Colors.amberAccent,
+      child: ListView(
+        children: [Column(
+          children: [Row(mainAxisAlignment: MainAxisAlignment.start,
+          children: [],
+          )
+          ],
+        )],
       ),
     );
   }
